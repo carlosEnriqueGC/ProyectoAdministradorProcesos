d3f87ca6109c2c9b3dd22c6b0df2aea61695585b
carlos enrique <cguzmanc10@miumg.edu.gt>
2022-09-22 20:58:07 -0600

    recontruccion de proyectos por problemas desconocidos

---
+++ administradorRoundRobin/src/administradorroundrobin/AdministradorRoundRobin.java
@@ -0,0 +1,19 @@
+
+package administradorroundrobin;
+
+/**
+ *
+ * @author callo
+ */
+public class AdministradorRoundRobin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        proceso ver = new proceso();
+        ver.setVisible(true);
+    }
+    
+}
